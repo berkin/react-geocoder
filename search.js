33f7e239246f946b4d466a906c92018c6de85a0c
berkin <berkin@kuyabiye.com>
2018-08-16 14:25:23 +0300

    upgrade react version

--- search.js
+++ search.js
@@ -1,19 +1,37 @@
-var xhr = require('xhr');
+var xhr = require("xhr");
 
-function search(endpoint, source, accessToken, proximity, bbox, types, query, callback) {
+function search(
+  endpoint,
+  source,
+  accessToken,
+  proximity,
+  bbox,
+  types,
+  query,
+  callback
+) {
   var searchTime = new Date();
-  var uri = endpoint + '/geocoding/v5/' +
-    source + '/' + encodeURIComponent(query) + '.json' +
-    '?access_token=' + accessToken +
-    (proximity ? '&proximity=' + proximity : '') +
-    (bbox ? '&bbox=' + bbox : '') +
-    (types ? '&types=' + encodeURIComponent(types) : '');
-  xhr({
-    uri: uri,
-    json: true
-  }, function(err, res, body) {
-    callback(err, res, body, searchTime);
-  });
+  var uri =
+    endpoint +
+    "/geocoding/v5/" +
+    source +
+    "/" +
+    encodeURIComponent(query) +
+    ".json" +
+    "?access_token=" +
+    accessToken +
+    (proximity ? "&proximity=" + proximity : "") +
+    (bbox ? "&bbox=" + bbox : "") +
+    (types ? "&types=" + encodeURIComponent(types) : "");
+  xhr(
+    {
+      uri: uri,
+      json: true
+    },
+    function(err, res, body) {
+      callback(err, res, body, searchTime);
+    }
+  );
 }
 
 module.exports = search;
